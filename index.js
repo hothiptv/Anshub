05960efac1a7db036ffea262a4272882559d0d10
hothiptv <hothiptvnek@gmail.com>
2026-01-18 14:42:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const WebSocket = require('ws');
 const http = require('http');
 
-const server = http.createServer((req, res) => { res.end('Anscript Cloud Engine'); });
+const server = http.createServer((req, res) => {
+    res.end('Anshub Cloud Engine v0.5 is Running!');
+});
+
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
     ws.on('message', (data) => {
-        // Phát tán lệnh JSON tới mọi client (Roblox sẽ tự check tên mình)
-        const rawData = data.toString();
+        const message = data.toString();
+        // Phát lệnh tới tất cả các bên (Web nhận để cập nhật UI, Roblox nhận để hack)
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(rawData);
+                client.send(message);
             }
         });
     });
 });
 
 server.listen(process.env.PORT || 8080);
- 
